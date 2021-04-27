deda06393f1f3c3f348429b8674cb0b6de1c4157
JeffreyMoreta <jeffreymoreta@gmail.com>
2021-04-27 11:27:04 -0400

    "Modified to accommodate the gameplay loop"

--- src/com.practical.jajamen.oldman/OpenScreen.java
+++ src/com.practical.jajamen.oldman/OpenScreen.java
@@ -15,16 +15,17 @@ public class OpenScreen {
     }
 
     public void printInstructions() {
-        System.out.println("\n Press ");
-        System.out.println("\t 1 - Start the game!");
-        System.out.println("\t 2 - Learn how awesome the creators are!");
-        System.out.println("\t 3 - Quit and miss out on greatness");
+        System.out.println("\n Type one of the following ");
+        System.out.println("\t play - Start the game!");
+        System.out.println("\t read - Learn how awesome the creators are!");
+        System.out.println("\t quit - Quit and miss out on greatness");
     }
 
     //Game starts when user pressed #1 and the intro to the game will begin
-    public void startGame() {
+    public void introduction() {
 
-        String text = "Hey, It’s your favorite antihero Deadpool here!\n" +
+        String text =
+                "Hey, It’s your favorite antihero Deadpool here!\n" +
                 "You’re probably wondering why I’m here..and no, I’m not the jack ass Deadpool from X-Men Origins.\n" +
                 "God, I hate that guy.\n" +
                 "Anywhooooooo, we are going on a little journey and we get to follow one of my FAVORITE superheros!\n" +
@@ -44,11 +45,10 @@ public class OpenScreen {
 
     public void showInstructions() {
         System.out.println("Commands:\n" +
-                "  go [north, south, east, west]\n" +
+                "  go [location name]\n" +
                 "  get [steroid]\n" +
                 "  fight [villain name]\n" +
-                "  run\n" +
-                "  q [to quit the game]");
+                "  quit [to quit the game]");
     }
 
     public void creatorDescription() {
