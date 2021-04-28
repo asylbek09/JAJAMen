1496f0878e3f1e7c96f99d3c7f0bf50c5dc888bb
asylbek09 <asylbek09@gmail.com>
2021-04-28 16:15:33 -0400

    Changed sout to inputParser, end the fight when one of the mutants dies, and declare winner, AB#254

--- src/com.practical.jajamen.oldman/Combat.java
+++ src/com.practical.jajamen.oldman/Combat.java
@@ -10,34 +10,39 @@ public class Combat {
     // TODO: Declare a winner, the user won't know who won otherwise
     // TODO: Refactor in to several methods
     // TODO: Catch and handle the error that is being thrown
-    // TODO: Change the souts to instead print from the inputParser
+    InputParser ip;
+
     public Combat(Character hero, Character villain) throws IOException {
+        ip = new InputParser();
+
         int hero_health = hero.getHealth();
         int villain_health = villain.getHealth();
-        System.out.println("You just encountered: " + villain.getName() + "\n");
-        System.out.println("Battle with " + villain.getName() + " starts\n");
+        ip.displayTextStream("You just encountered: " + villain.getName() + "\n");
+        ip.displayTextStream("Battle with " + villain.getName() + " starts\n");
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
         while (hero_health > 0 || villain_health > 0) {
-            System.out.print("Enter (p) to use your power or heal (h)? ");
-            String action = in.readLine();
-            System.out.println(
-                    "Your health: " + hero_health + " , power and damage: " + hero.getPower() + "\n" +
+            ip.displayTextStream("Your health: " + hero_health + " , power and damage: " + hero.getPower() + "\n" +
                             villain.getName() + "'s health: " + villain_health + " , power and damage: " + villain.getPower() + "\n");
+            ip.displayTextStream("Enter (p) to use your power or heal (h)? ");
+            String action = in.readLine();
+
             if (action.equals("h")) {
                 hero.setHealth(hero.getSteroid() + hero.getHealth());
-            } else if (action.equals("p")) {
-                if(hero_health < 0) {
-                    System.out.println("Ups, GAME OVER, you were killed by " + villain.getName() + "!");
-                    break;
-                }
-                if(villain_health < 0){
-                    System.out.println("Congratulations! You just killed " + villain.getName());
-                    break;
-                }
+            }
+            if (action.equals("p")) {
                 villain_health -= hero.getPowerLimit();
                 hero_health -= villain.getPowerLimit();
+                if(hero_health <= 0) {
+                    ip.displayTextStream("Ups, GAME OVER, you were killed by " + villain.getName() + "!");
+                    System.exit(0);
+                }
+                if(villain_health <= 0){
+                    ip.displayTextStream("Congratulations! You just killed " + villain.getName());
+                    System.exit(0);
+                }
             } else {
-                System.out.println("Enter (p) to use your power or heal (h)? ");
+                ip.displayTextStream("Enter (p) to use your power or heal (h)? ");
             }
         }
     }
